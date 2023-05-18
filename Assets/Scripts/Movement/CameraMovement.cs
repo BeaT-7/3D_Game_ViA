b5de0e48dc8d068689bd74b2a3c7fc03374a1319
BeaT <miksipro11@gmail.com>
2023-05-18 04:51:55 +0300

    Player movement complete

--- Assets/Scripts/Movement/CameraMovement.cs
+++ Assets/Scripts/Movement/CameraMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] float sensX;
-    [SerializeField] float sensY;
+    public float sensX;
+    public float sensY;
 
     public Transform orientation;
 
@@ -23,19 +23,16 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * sensY;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
+       
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        xRotation -= Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-        Debug.Log("xAxis " + xRotation);
-        Debug.Log("yAxis " + yRotation);
-
 
     }
 }
